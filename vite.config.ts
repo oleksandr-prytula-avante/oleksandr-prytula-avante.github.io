40ed5b81e8960b66703c72d3210d45aa2604e517
Oleksandr Prytula <oleksandr.prytula.avante@gmail.com>
2026-03-07 18:46:19 +0200

    fix: use relative base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(function () {
   return {
-    base: "/",
+    base: "./",
     plugins: [react(), tailwindcss()],
   };
 });
